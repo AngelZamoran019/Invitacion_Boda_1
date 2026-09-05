74aeff4780eed7643d0a9262e903c464a84bf3e7
AngelZamoran019 <azamorano017@gmail.com>
2026-09-05 16:02:00 -0600

    Synchronize embedded wedding fonts in VP and export

--- src/export/getWeddingExportHTML.js
+++ src/export/getWeddingExportHTML.js
@@ -1,11 +1,13 @@
 import { renderWeddingHTML } from './weddingOutput.js'
 import { applyCoverPositionsToExport } from './applyCoverPositions.js'
 import { applyWeddingEditorParity } from './applyWeddingEditorParity.js'
+import { embedWeddingFonts } from './embedWeddingFonts.js'
 
-export function getWeddingExportHTML(project) {
+export async function getWeddingExportHTML(project) {
   const rendered = renderWeddingHTML(project)
   const withCoverSync = applyCoverPositionsToExport(rendered, project)
-  return applyWeddingEditorParity(withCoverSync, project)
+  const withEditorParity = applyWeddingEditorParity(withCoverSync, project)
+  return embedWeddingFonts(withEditorParity)
 }
 
 export default getWeddingExportHTML
