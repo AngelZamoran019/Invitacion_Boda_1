7e2c59ab79de53d0773c387b53e053d86a9a1ba2
AngelZamoran019 <azamorano017@gmail.com>
2026-09-05 15:33:34 -0600

    fix: complete editor-to-VP-export synchronization

--- src/export/getWeddingExportHTML.js
+++ src/export/getWeddingExportHTML.js
@@ -1,8 +1,11 @@
 import { renderWeddingHTML } from './weddingOutput.js'
 import { applyCoverPositionsToExport } from './applyCoverPositions.js'
+import { applyWeddingEditorParity } from './applyWeddingEditorParity.js'
 
 export function getWeddingExportHTML(project) {
-  return applyCoverPositionsToExport(renderWeddingHTML(project), project)
+  const rendered = renderWeddingHTML(project)
+  const withCoverSync = applyCoverPositionsToExport(rendered, project)
+  return applyWeddingEditorParity(withCoverSync, project)
 }
 
 export default getWeddingExportHTML
