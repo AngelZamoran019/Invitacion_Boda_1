a5515301533a5ca76e0c66b0600f16f10d81af1b
AngelZamoran019 <azamorano017@gmail.com>
2026-09-06 16:50:56 -0600

    fix: restore editor and preview stability before scroll animation integration

--- src/export/getWeddingExportHTML.js
+++ src/export/getWeddingExportHTML.js
@@ -2,14 +2,12 @@ import { renderWeddingHTML } from './weddingOutput.js'
 import { applyCoverPositionsToExport } from './applyCoverPositions.js'
 import { applyWeddingEditorParity } from './applyWeddingEditorParity.js'
 import { embedWeddingFonts } from './embedWeddingFonts.js'
-import { applyWeddingScrollAnimations } from './applyWeddingScrollAnimations.js'
 
 export async function getWeddingExportHTML(project) {
   const rendered = renderWeddingHTML(project)
   const withCoverSync = applyCoverPositionsToExport(rendered, project)
   const withEditorParity = applyWeddingEditorParity(withCoverSync, project)
-  const withFonts = embedWeddingFonts(withEditorParity)
-  return applyWeddingScrollAnimations(withFonts)
+  return embedWeddingFonts(withEditorParity)
 }
 
 export default getWeddingExportHTML
