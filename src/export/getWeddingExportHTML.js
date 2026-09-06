43de410c52718e59a46723008c1cf775724f89c3
AngelZamoran019 <azamorano017@gmail.com>
2026-09-06 16:42:16 -0600

    feat: connect scroll reveal animations to wedding HTML

--- src/export/getWeddingExportHTML.js
+++ src/export/getWeddingExportHTML.js
@@ -2,12 +2,14 @@ import { renderWeddingHTML } from './weddingOutput.js'
 import { applyCoverPositionsToExport } from './applyCoverPositions.js'
 import { applyWeddingEditorParity } from './applyWeddingEditorParity.js'
 import { embedWeddingFonts } from './embedWeddingFonts.js'
+import { applyWeddingScrollAnimations } from './applyWeddingScrollAnimations.js'
 
 export async function getWeddingExportHTML(project) {
   const rendered = renderWeddingHTML(project)
   const withCoverSync = applyCoverPositionsToExport(rendered, project)
   const withEditorParity = applyWeddingEditorParity(withCoverSync, project)
-  return embedWeddingFonts(withEditorParity)
+  const withFonts = embedWeddingFonts(withEditorParity)
+  return applyWeddingScrollAnimations(withFonts)
 }
 
 export default getWeddingExportHTML
